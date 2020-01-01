f40aa78dedd46d4cffdb8c935144aac65c97b405
larstomas <larstomas@gmail.com>
2020-01-01 23:20:44 +0100

    solution to #1

--- 0-code-snippets/Python-Unit-Testing/test_calc.py
+++ 0-code-snippets/Python-Unit-Testing/test_calc.py
@@ -5,7 +5,7 @@ import calc
 class TestCalc(unittest.TestCase):
 
     def test_add(self):
-        self.assertEqual(calc.add(10, 5), 14)
+        self.assertEqual(calc.add(10, 5), 15)
         self.assertEqual(calc.add(-1, 1), 0)
         self.assertEqual(calc.add(-1, -1), -2)
 
--- number-1/return-when-sum-is-k.py
+++ number-1/return-when-sum-is-k.py
@@ -1,1 +1,16 @@
-print('Hello World')
+list_of_numbers = [10, 15, 3, 7]
+k = 17
+
+print(len(list_of_numbers))
+
+def two_sum(list_of_numbers, k):
+    k_friends = []  # A k_friend
+    for num in list_of_numbers:
+        if num in k_friends:
+            print("True, numbers: " + str(num) + " " + str(k - num))
+            return True
+        k_friends.append(k - num)
+    return False
+
+
+print(two_sum(list_of_numbers, k))
