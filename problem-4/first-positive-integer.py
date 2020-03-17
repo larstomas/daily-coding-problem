af0a3afeebde2eb057b59c3ee67b3a88c726a69a
larstomas <larstomas@gmail.com>
2020-03-17 23:14:07 +0100

    3

---
+++ problem-4/first-positive-integer.py
@@ -0,0 +1,17 @@
+
+def first_missing_int(array):
+    first_missing = None
+    for i in range(len(array)):
+    for num in array
+        if (first_missing is None):
+             and (num = 1):
+            fi
+            
+    return first_missing
+
+# Inpot node
+input_array = [3, 4, -1, 1]
+
+print(first_missing_int(input_array))
+
+
